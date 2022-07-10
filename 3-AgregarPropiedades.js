e86f43f6226bb21557d5d9a7f286dfecfa852de4
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2022-07-10 14:26:40 -0400

    07/10/2022

---
+++ 3-AgregarPropiedades.js
@@ -0,0 +1,13 @@
+var producto = {
+    "nombre": "Yogurt Light",
+    "precio": 6.50,
+    "marca": "Yogumon"
+};
+
+//Agregando una nueva propiedad
+producto.cantidad = 950;
+console.log(producto.cantidad);
+
+//Agregando una nueva propiedad
+producto["calorias"] = 300;
+console.log(producto.calorias);
