8b406197e2c8251139be8dd18246d931409065ff
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2022-07-10 14:15:59 -0400

    07/1/2022

---
+++ 2-ActualizarPropiedades.js
@@ -0,0 +1,19 @@
+var auto = {
+    "modelo": "Lamborghini",
+    "color": "rojo",
+    "kilometraje": 0,
+}
+
+console.log("Datos Actuales");
+console.log(auto.modelo);
+console.log(auto.color);
+console.log(auto.kilometraje);
+
+//Actualizando las propiedades del auto
+auto.modelo = "Ferrari"; 
+auto.color = "Azul";
+auto.kilometraje = 15;
+console.log("\nDatos Nuevos");
+console.log(auto.modelo);
+console.log(auto.color);
+console.log(auto.kilometraje);
