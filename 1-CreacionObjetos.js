23f54a25edb40522547f09b93ae1fcb355580efe
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2022-07-10 13:58:35 -0400

    1-CreacionObjetos

---
+++ 1-CreacionObjetos.js
@@ -0,0 +1,16 @@
+var persona = {//Nombre del objecto
+    "nombre": "Edgar",
+    "edad"  :   18,
+    "genero": "Masculino"
+};
+
+var ganadores = {//Nombre del objecto
+    1: "Juan",
+    2: "Alex",
+    3: "Sebastian"
+};
+
+console.log(persona.nombre);//Accediendo al campo nombre
+console.log(persona["edad"]);//Accediendo al campo edad
+
+console.log(ganadores[1]);//Accediendo al  campo 1
