6fcc60f2f31f995a877349ec5127a81e57dc8317
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2022-07-13 12:07:43 -0400

    07/13/2022

---
+++ 9-ArreglosAnidados.js
@@ -0,0 +1,21 @@
+var plantas = [
+    {
+    tipo: "flores",
+    lista: [
+        "rosas",
+        "tulipanes",
+        "margaritas"
+    ]
+    },
+    {
+        tipo: "arboles",
+        lista: [
+            "abeto",
+            "pino",
+            "abedul"
+        ]
+    }
+];
+
+console.log(plantas[0].lista[0]);
+console.log(plantas[1].lista[1]);
